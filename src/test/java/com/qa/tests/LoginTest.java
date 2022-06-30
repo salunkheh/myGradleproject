f125add06ba570588780cdbb81cbd6ad21eb640a
salunkheh <salunkheharish@gmail.com>
2022-06-30 11:14:42 +0530

    commenting driver set property

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -27,7 +27,7 @@ public class LoginTest {
 	public void setup() {
 		
 		try {
-  		  	System.setProperty("webdriver.gecko.driver","Drivers/geckodriver");
+  		  	//System.setProperty("webdriver.gecko.driver","Drivers/geckodriver");
 		   // driver = new FirefoxDriver();
   		 
   		  	
